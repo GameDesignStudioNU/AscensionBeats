e5670faaf2117fc340108bee25aed2f023f204c5
Dennis Angelov <denangelov123@gmail.com>
2020-02-12 20:03:38 -0600

    implemented basic movement and wall-jumping mechanic

---
+++ Assets/Code/SpriteManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteManager : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private Rigidbody2D _rb;
+    private float epsilon = .05f;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        _rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        if (_rb.velocity.x < -epsilon)
+        {
+            sprite.flipX = true;
+        }
+
+        if (_rb.velocity.x > epsilon)
+        {
+            sprite.flipX = false;
+        }
+    }
+}
